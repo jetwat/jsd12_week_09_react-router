248c2acdb177e6ba35c54fd11a70c243c4266aae
jetwat <jetwatwarathip@gmail.com>
2026-04-27 11:10:11 +0700

    Created router and Layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/Layout";
+// createBrowserRouter รับพารามิเตอร์เป็น array
+const router = createBrowserRouter([
+  {
+  path: "/",
+  element: <Layout />,
+  }
+]);
+
 export default function App() {
-  return (
-    <div className="min-h-screen flex justify-center bg-blue-950">
-      <div className="p-6 gap-y-6 flex flex-col justify-start w-[80%] lg:w-[70%]">
-        <h1 className="w-full p-6 bg-amber-100 font-extrabold">
-          React App Starter
-        </h1>
-        <section className="w-full p-5 bg-amber-100 flex">
-          <ul className="list-inside list-disc flex-1">
-            <span className="font-semibold">Tech Stack:</span>
-            <li>Vite</li>
-            <li>React</li>
-            <li>JavaScript</li>
-            <li>Tailwind</li>
-          </ul>
-        </section>
-      </div>
-    </div>
-  );
+  return <RouterProvider router={router}/>;
 }
